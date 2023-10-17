cb52c2d2a3d45f229899b1dfc519c1fc3ed1bf97
ViniciusOliver-stack <vinicius.so.contato@hotmail.com>
2023-10-16 21:30:28 -0300

    :sparkles:	feat: route structure

--- 02-ignite-timer/src/App.tsx
+++ 02-ignite-timer/src/App.tsx
@@ -1,7 +1,10 @@
+import { BrowserRouter } from 'react-router-dom'
+import { Router } from './Router'
+
 export function App() {
   return (
-    <div className="h-screen bg-gray_color-700">
-      <h1>Hello, world</h1>
-    </div>
+    <BrowserRouter>
+      <Router />
+    </BrowserRouter>
   )
 }
