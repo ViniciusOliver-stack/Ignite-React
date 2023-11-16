9cf0646467b66a6ef0c6d2e845ae32ad11766f9f
ViniciusOliver-stack <vinicius.so.contato@hotmail.com>
2023-11-15 21:26:19 -0300

    feat: display the value entered by the user

--- 02-ignite-timer/src/pages/home/index.tsx
+++ 02-ignite-timer/src/pages/home/index.tsx
@@ -1,5 +1,6 @@
 import { zodResolver } from '@hookform/resolvers/zod'
 import { Play } from '@phosphor-icons/react'
+import { useState } from 'react'
 import { useForm } from 'react-hook-form'
 import * as zod from 'zod'
 
@@ -13,7 +14,17 @@ const newCycleFormValidationSchema = zod.object({
 
 type NewCycleFormData = zod.infer<typeof newCycleFormValidationSchema>
 
+interface Cycle {
+  id: string
+  task: string
+  minutesAmount: number
+}
+
 export function Home() {
+  const [cycles, setCycles] = useState<Cycle[]>([])
+  const [activeCycleId, setActiveCycleId] = useState<string | null>(null)
+  const [amountSecondsPassed, setAmountSecondsPassed] = useState(0)
+
   const {
     register,
     handleSubmit,
@@ -25,8 +36,28 @@ export function Home() {
 
   const isSubmitDisabled = !isValid
 
+  const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId)
+
+  const totalSeconds = activeCycle ? activeCycle.minutesAmount * 60 : 0
+  const currentSeconds = activeCycle ? totalSeconds - amountSecondsPassed : 0
+
+  const minutesAmount = currentSeconds / 60
+  const secondsAmount = currentSeconds % 60
+
+  const minutes = String(minutesAmount).padStart(2, '0')
+  const seconds = String(secondsAmount).padStart(2, '0')
+
   function handleCreateNewCycle(data: NewCycleFormData) {
-    console.log(data)
+    const id = String(new Date().getTime())
+    const newCycle: Cycle = {
+      id,
+      task: data.task,
+      minutesAmount: data.minutesAmount,
+    }
+
+    setCycles((state) => [...state, newCycle])
+    setActiveCycleId(id)
+
     reset()
   }
 
@@ -66,11 +97,19 @@ export function Home() {
         </div>
 
         <div className="text-[10rem] text-gray_color-100 flex gap-4 font-mono">
-          <span className="bg-gray_color-600 px-4 rounded-lg">0</span>
-          <span className="bg-gray_color-600 px-4 rounded-lg">0</span>
+          <span className="bg-gray_color-600 px-4 rounded-lg">
+            {minutes[0]}
+          </span>
+          <span className="bg-gray_color-600 px-4 rounded-lg">
+            {minutes[1]}
+          </span>
           <span className="text-green_color-500">:</span>
-          <span className="bg-gray_color-600 px-4 rounded-lg">0</span>
-          <span className="bg-gray_color-600 px-4 rounded-lg">0</span>
+          <span className="bg-gray_color-600 px-4 rounded-lg">
+            {seconds[0]}
+          </span>
+          <span className="bg-gray_color-600 px-4 rounded-lg">
+            {seconds[1]}
+          </span>
         </div>
 
         <button
