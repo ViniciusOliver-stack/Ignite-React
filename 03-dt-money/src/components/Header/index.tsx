312e32ace1a5112d9092783a26f7c3263629d6ae
Vinicius Oliveira <vinicius.so.contato@hotmail.com>
2024-04-16 20:51:39 -0300

    feat: create dialog transactions

--- 03-dt-money/src/components/Header/index.tsx
+++ 03-dt-money/src/components/Header/index.tsx
@@ -1,4 +1,12 @@
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog"
 import { Button } from "../ui/button"
+import { ArrowCircleDown, ArrowCircleUp } from "phosphor-react"
 
 export function Header() {
   return (
@@ -10,11 +18,74 @@ export function Header() {
             <span className="text-2xl text-white font-bold">DT Money</span>
           </div>
 
-          <Button className="bg-green-600 text-white font-bold py-5 hover:bg-green-700">
-            Nova transação
-          </Button>
+          <Dialog>
+            <DialogTrigger asChild>
+              <Button className="bg-green-600 text-white font-bold py-5 hover:bg-green-700">
+                Nova transação
+              </Button>
+            </DialogTrigger>
+            <DialogContent className="bg-[#202024] text-white border-none">
+              <DialogHeader className="">
+                <DialogTitle>Nova transação</DialogTitle>
+                <form className="pt-8 flex flex-col gap-4">
+                  <input
+                    type="text"
+                    placeholder="Descrição"
+                    className="w-full rounded-md bg-gray-900 p-4 gray-300 placeholder:text-gray-500 focus:outline-none"
+                  />
+                  <input
+                    type="number"
+                    placeholder="Valor"
+                    className="w-full rounded-md bg-gray-900 p-4 gray-300 placeholder:text-gray-500 focus:outline-none"
+                  />
+                  <input
+                    type="text"
+                    placeholder="Categoria"
+                    className="w-full rounded-md bg-gray-900 p-4 gray-300 placeholder:text-gray-500 focus:outline-none"
+                  />
+
+                  <div className="grid grid-cols-2 gap-4 mt-2">
+                    <ButtonNewTransaction transactionType="income">
+                      <ArrowCircleUp size={24} />
+                      Entrada
+                    </ButtonNewTransaction>
+                    <ButtonNewTransaction transactionType="outcome">
+                      <ArrowCircleDown size={24} />
+                      Saída
+                    </ButtonNewTransaction>
+                  </div>
+
+                  <button className="h-[58px] border-none bg-green-700 py-3 rounded-md mt-6 cursor-pointer hover:bg-green-800 hover:text-white transition-all">
+                    Cadastrar
+                  </button>
+                </form>
+              </DialogHeader>
+            </DialogContent>
+          </Dialog>
         </div>
       </div>
     </>
+  )
+}
+
+interface NewTransactionProps {
+  transactionType: "income" | "outcome"
+  children: React.ReactNode
+}
+
+function ButtonNewTransaction({
+  transactionType,
+  children,
+}: NewTransactionProps) {
+  return (
+    <Button
+      className={`bg-gray-700 p-4 flex items-center justify-center gap-2 rounded-md cursor-pointer border-none text-gray-300 ${
+        transactionType === "income"
+          ? "hover:bg-green-500 hover:text-white"
+          : "hover:bg-red-500 hover:text-white"
+      }`}
+    >
+      {children}
+    </Button>
   )
 }
