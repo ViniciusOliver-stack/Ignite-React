15feabcf0f9ca36790b64467eb2cd968d9c79220
Vinicius Oliveira <vinicius.so.contato@hotmail.com>
2024-04-24 19:36:06 -0300

    feat: create radio check transaction

--- 03-dt-money/src/components/Header/index.tsx
+++ 03-dt-money/src/components/Header/index.tsx
@@ -5,10 +5,15 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog"
+
 import { Button } from "../ui/button"
+
 import { ArrowCircleDown, ArrowCircleUp } from "phosphor-react"
+import { useState } from "react"
 
 export function Header() {
+  const [typeTransaction, setTypeTransaction] = useState<string>("")
+
   return (
     <>
       <div className="bg-gray-900 pt-10 pb-28">
@@ -44,16 +49,44 @@ export function Header() {
                     className="w-full rounded-md bg-gray-900 p-4 gray-300 placeholder:text-gray-500 focus:outline-none"
                   />
 
-                  <div className="grid grid-cols-2 gap-4 mt-2">
-                    <ButtonNewTransaction transactionType="income">
-                      <ArrowCircleUp size={24} />
-                      Entrada
-                    </ButtonNewTransaction>
-                    <ButtonNewTransaction transactionType="outcome">
-                      <ArrowCircleDown size={24} />
-                      Saída
-                    </ButtonNewTransaction>
-                  </div>
+                  <ul className="grid w-full gap-6 md:grid-cols-2">
+                    <li>
+                      <input
+                        type="checkbox"
+                        name="typeTransaction"
+                        id="entrada"
+                        value=""
+                        className="hidden peer"
+                        checked={typeTransaction === "entrada"}
+                        onChange={() => setTypeTransaction("entrada")}
+                      />
+                      <label
+                        htmlFor="entrada"
+                        className="inline-flex items-center gap-2 w-full p-5 border border-neutral-600 text-gray-500  rounded-lg cursor-pointer   peer-checked:bg-green-500 hover:text-white peer-checked:text-white hover:bg-green-500"
+                      >
+                        <ArrowCircleUp size={24} />
+                        Entrada
+                      </label>
+                    </li>
+                    <li>
+                      <input
+                        type="checkbox"
+                        name="typeTransaction"
+                        id="saida"
+                        value=""
+                        className="hidden peer"
+                        checked={typeTransaction === "saida"}
+                        onChange={() => setTypeTransaction("saida")}
+                      />
+                      <label
+                        htmlFor="saida"
+                        className="inline-flex items-center gap-2 w-full p-5 border border-neutral-600 text-gray-500  rounded-lg cursor-pointer   peer-checked:bg-red-500 hover:text-white peer-checked:text-white hover:bg-red-500"
+                      >
+                        <ArrowCircleDown size={24} />
+                        Saída
+                      </label>
+                    </li>
+                  </ul>
 
                   <button className="h-[58px] border-none bg-green-700 py-3 rounded-md mt-6 cursor-pointer hover:bg-green-800 hover:text-white transition-all">
                     Cadastrar
@@ -68,24 +101,31 @@ export function Header() {
   )
 }
 
-interface NewTransactionProps {
-  transactionType: "income" | "outcome"
-  children: React.ReactNode
-}
+// interface NewTransactionProps {
+//   transactionType: "income" | "outcome"
+//   children: React.ReactNode
+// }
 
-function ButtonNewTransaction({
-  transactionType,
-  children,
-}: NewTransactionProps) {
-  return (
-    <Button
-      className={`bg-gray-700 p-4 flex items-center justify-center gap-2 rounded-md cursor-pointer border-none text-gray-300 ${
-        transactionType === "income"
-          ? "hover:bg-green-500 hover:text-white"
-          : "hover:bg-red-500 hover:text-white"
-      }`}
-    >
-      {children}
-    </Button>
-  )
-}
+// function ButtonNewTransaction({
+//   transactionType,
+//   children,
+// }: NewTransactionProps) {
+//   return (
+//     <RadioGroup defaultValue="option-one">
+//       <div className="flex items-center space-x-2">
+//         <RadioGroupItem value="option-one" id="option-one" className="hidden" />
+//         <Label htmlFor="option-one">
+//           <ArrowCircleUp size={24} />
+//           Entrada
+//         </Label>
+//       </div>
+//       <div className="flex items-center space-x-2">
+//         <RadioGroupItem value="option-two" id="option-two" className="hidden" />
+//         <Label htmlFor="option-two">
+//           <ArrowCircleDown size={24} />
+//           Saída
+//         </Label>
+//       </div>
+//     </RadioGroup>
+//   )
+// }
